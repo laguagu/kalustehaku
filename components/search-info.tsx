c53b45c5be3aad7d8909c2963c41dbccbaa7df3c
laguagu <49702497+laguagu@users.noreply.github.com>
2024-11-29 16:16:13 +0200

    style: Modify information texts

--- components/search-info.tsx
+++ components/search-info.tsx
@@ -12,33 +12,27 @@ export function SearchInfo() {
       <Accordion type="single" collapsible className="w-full">
         <AccordionItem value="search-info" className="border-none">
           <AccordionTrigger className="p-0 hover:no-underline text-blue-900 font-medium">
-            Näin älykäs hakumme toimii
+            Vinkkejä hakuun
           </AccordionTrigger>
           <AccordionContent>
             <ul className="list-disc pl-4 space-y-1 mt-2 text-muted-foreground">
               <li>
-                Tekoäly analysoi hakusanasi ja vertaa sitä tietokantamme
-                tuotteisiin. Haku perustuu tuotteiden ominaisuuksiin ja
-                kuvauksiin - ei tuotemerkkeihin tai malleihin.
+                Tekoäly ymmärtää luonnollista kieltä - voit kuvailla haluamaasi
+                huonekalua vapaasti (esim. &quot;Väriltään valkoinen 
+                toimistopöytä&quot;)
               </li>
               <li>
-                Koska kyseessä on testiversio, tuotevalikoima on vielä
-                rajallinen. Hakiessasi voit kuvailla esimerkiksi tuotteen
-                tyyliä, väriä, materiaalia tai käyttötarkoitusta.
-              </li>
-              <li>
-                Näytämme 6 parhaiten vastaavaa tuotetta vastaavuusprosentin
-                kanssa. 100% tarkoittaa täydellistä osumaa, 42% heikointa
-                mahdollista näytettävää osumaa.
+                Rajaa hakutuloksia käyttämällä filttereitä kategorian, materiaalien 
+                ja värien mukaan
               </li>
               <li>
                 Vihreällä korostettu tuote on parhaiten hakuasi vastaava osuma
-                tämänhetkisestä valikoimastamme.
+                tämänhetkisestä valikoimastamme
               </li>
               <li>
                 Jos hakemaasi tuotetta ei löydy suoraan, näytämme samankaltaisia
                 vaihtoehtoja. Listan viimeiset tulokset voivat poiketa haustasi
-                merkittävästi johtuen rajallisesta tuotevalikoimastamme.
+                johtuen rajallisesta tuotevalikoimastamme.
               </li>
             </ul>
           </AccordionContent>
